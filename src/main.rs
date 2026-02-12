f4289de1115c42718552b3b52bd3982f85c94a7d
Logan Rose <logprose@umich.edu>
2026-02-11 23:18:20 -0500

    feat: added command enum for basic project structure

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,18 @@ struct Args {
     #[arg(short, long)]
     bucket: String,
 
+    // Do I even care about making this non-interactive?
     #[arg(short, long, default_value_t = false)]
     interactive: bool
+}
+
+#[derive(Parser, Debug)]
+enum Commands {
+    Copy,
+    Move,
+    List, // is it possible to add additional flags like -l?
+    ChangeDirectory,
+    Tree,
 }
 
 fn main() {
