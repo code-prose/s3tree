288f70cb758411caff5c01333979ee1508333d16
Logan Rose <logprose@umich.edu>
2026-03-10 21:26:25 -0400

    feat: setup aws s3 sdk

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,113 @@
 use clap::Parser;
 use std::io;
 use std::process::Command;
+use aws_sdk_s3 as s3;
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
-};
+// use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+// use ratatui::{
+//     buffer::Buffer,
+//     layout::Rect,
+//     style::Stylize,
+//     symbols::border,
+//     text::{Line, Text},
+//     widgets::{Block, Paragraph, Widget},
+//     DefaultTerminal, Frame,
+// };
 
 struct Directory {
     children: Vec<Box<Directory>>,
     parent: Box<Directory>,
 }
 
-#[derive(Debug, Default)]
-pub struct App {
-    curr_dir: String,
-    exit: bool
-}
-
-impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Char('j') => todo!(),
-            KeyCode::Char('k') => todo!(),
-            KeyCode::Enter => {
-                todo!()
-                // do I have to handle something here regarding what is hovered?
-            }
-            // I want to handle j - k - up -down for navigation between buckets
-            // I also want to handle enter... this could depend on whether the person is hovering
-            // ../ or a directory... or even an item... Could I make it so a person could read?
-            // Would I do this by doing aws s3 cp into some buffer and then displaying it? Then
-            // what if the person wants to make changes? Could I also enable this by writing the
-            // buffer back out to s3?
-            _ => {}
-        }
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-
-    fn current_directory(&mut self, new: String) {
-        self.curr_dir = new;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Counter App Tutorial ".bold());
-        let instructions = Line::from(vec![
-            " Down ".into(),
-            "<j / down>".blue().bold(),
-            " Up ".into(),
-            "<k / up>".blue().bold(),
-            " Enter directory ".into(),
-            "<Enter>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        // let block = Block::bordered()
-        //     .title(title.centered())
-        //     .title_bottom(instructions.centered())
-        //     .border_set(border::THICK);
-        //
-        // let counter_text = Text::from(vec![Line::from(vec![
-        //     "Value: ".into(),
-        //     self.counter.to_string().yellow(),
-        // ])]);
-        //
-        // Paragraph::new(counter_text)
-        //     .centered()
-        //     .block(block)
-        //     .render(area, buf);
-    }
-}
-
+// #[derive(Debug, Default)]
+// pub struct App {
+//     curr_dir: String,
+//     exit: bool
+// }
+//
+// impl App {
+//     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+//         while !self.exit {
+//             terminal.draw(|frame| self.draw(frame))?;
+//             self.handle_events()?;
+//         }
+//         Ok(())
+//     }
+//
+//     fn draw(&self, frame: &mut Frame) {
+//         frame.render_widget(self, frame.area());
+//     }
+//
+//     fn handle_events(&mut self) -> io::Result<()> {
+//         match event::read()? {
+//             // it's important to check that the event is a key press event as
+//             // crossterm also emits key release and repeat events on Windows.
+//             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+//                 self.handle_key_event(key_event)
+//             }
+//             _ => {}
+//         };
+//         Ok(())
+//     }
+//
+//     fn handle_key_event(&mut self, key_event: KeyEvent) {
+//         match key_event.code {
+//             KeyCode::Char('q') => self.exit(),
+//             KeyCode::Char('j') => todo!(),
+//             KeyCode::Char('k') => todo!(),
+//             KeyCode::Enter => {
+//                 todo!()
+//                 // do I have to handle something here regarding what is hovered?
+//             }
+//             // I want to handle j - k - up -down for navigation between buckets
+//             // I also want to handle enter... this could depend on whether the person is hovering
+//             // ../ or a directory... or even an item... Could I make it so a person could read?
+//             // Would I do this by doing aws s3 cp into some buffer and then displaying it? Then
+//             // what if the person wants to make changes? Could I also enable this by writing the
+//             // buffer back out to s3?
+//             _ => {}
+//         }
+//     }
+//
+//     fn exit(&mut self) {
+//         self.exit = true;
+//     }
+//
+//     fn current_directory(&mut self, new: String) {
+//         self.curr_dir = new;
+//     }
+// }
+//
+// impl Widget for &App {
+//     fn render(self, area: Rect, buf: &mut Buffer) {
+//         let title = Line::from(" Counter App Tutorial ".bold());
+//         let instructions = Line::from(vec![
+//             " Down ".into(),
+//             "<j / down>".blue().bold(),
+//             " Up ".into(),
+//             "<k / up>".blue().bold(),
+//             " Enter directory ".into(),
+//             "<Enter>".blue().bold(),
+//             " Quit ".into(),
+//             "<Q> ".blue().bold(),
+//         ]);
+//         // let block = Block::bordered()
+//         //     .title(title.centered())
+//         //     .title_bottom(instructions.centered())
+//         //     .border_set(border::THICK);
+//         //
+//         // let counter_text = Text::from(vec![Line::from(vec![
+//         //     "Value: ".into(),
+//         //     self.counter.to_string().yellow(),
+//         // ])]);
+//         //
+//         // Paragraph::new(counter_text)
+//         //     .centered()
+//         //     .block(block)
+//         //     .render(area, buf);
+//     }
+// }
+//
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
@@ -127,7 +128,7 @@ enum Commands {
     Tree,
 }
 
-fn arg_loop() {
+async fn arg_loop(client: &aws_sdk_s3::Client, bucket: &str) -> Result<(), s3::Error> {
     loop {
         let mut cmd = String::new();
         io::stdin().read_line(&mut cmd).expect("Failed to parse command");
@@ -136,8 +137,9 @@ fn arg_loop() {
             "exit" => break,
             "ls" => {
                 // -a? -l?
-                let res = Command::new("ls").spawn();
-                println!("{res:?}");
+                // let res = Command::new("ls").spawn();
+                // println!("{res:?}");
+                list_bucket(client, bucket).await?;
             },
             "cd" => {
                 // cd foo/bar/?
@@ -158,58 +160,121 @@ fn arg_loop() {
             
         }
     }
+    Ok(())
 }
 
-// fn main() {
-//     let args = Args::parse();
-//     println!("{args:?}");
+#[::tokio::main]
+async fn main() -> Result<(), s3::Error> {
+    let args = Args::parse();
+    println!("{args:?}");
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_s3::Client::new(&config);
+    // Will need to handle some type of exceptions here... Might want to handle in the arg loop
+    let result = arg_loop(&client, &args.bucket).await;
+
+    Ok(())
+}
+
+
+
+async fn list_bucket(
+    client: &aws_sdk_s3::Client,
+    bucket: &str,
+) -> Result<(), s3::Error> {
+    // List the buckets in this account
+    let mut objects = client
+        .list_objects_v2()
+        .bucket(bucket)
+        .into_paginator()
+        .send();
+
+    println!("key\tetag\tlast_modified\tstorage_class");
+    while let Some(Ok(object)) = objects.next().await {
+        for item in object.contents() {
+            println!(
+                "{}\t{}\t{}\t{}",
+                item.key().unwrap_or_default(),
+                item.e_tag().unwrap_or_default(),
+                item.last_modified()
+                    .map(|lm| format!("{lm}"))
+                    .unwrap_or_default(),
+                item.storage_class()
+                    .map(|sc| format!("{sc}"))
+                    .unwrap_or_default()
+            );
+        }
+    }
+
+    // Prepare a ByteStream around the file, and upload the object using that ByteStream.
+    // let body = aws_sdk_s3::primitives::ByteStream::from_path(filepath)
+    //     .await
+    //     .map_err(|err| {
+    //         S3ExampleError::new(format!(
+    //             "Failed to create bytestream for {filepath:?} ({err:?})"
+    //         ))
+    //     })?;
+    // let resp = client
+    //     .put_object()
+    //     .bucket(bucket)
+    //     .key(key)
+    //     .body(body)
+    //     .send()
+    //     .await?;
+
+
+    // Retrieve the just-uploaded object.
+    // let resp = client.get_object().bucket(bucket).key(key).send().await?;
+    // println!("etag: {}", resp.e_tag().unwrap_or("(missing)"));
+    // println!("version: {}", resp.version_id().unwrap_or("(missing)"));
+
+    Ok(())
+}
+
+// fn main() -> io::Result<()> {
+//     ratatui::run(|terminal| App::default().run(terminal))
 // }
-
-fn main() -> io::Result<()> {
-    ratatui::run(|terminal| App::default().run(terminal))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ratatui::style::Style;
-
-    #[test]
-    fn render() {
-        let app = App::default();
-        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        app.render(buf.area, &mut buf);
-
-        let mut expected = Buffer::with_lines(vec![
-            "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
-            "┃                    Value: 0                    ┃",
-            "┃                                                ┃",
-            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        ]);
-        let title_style = Style::new().bold();
-        let counter_style = Style::new().yellow();
-        let key_style = Style::new().blue().bold();
-        expected.set_style(Rect::new(14, 0, 22, 1), title_style);
-        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-        assert_eq!(buf, expected);
-    }
-
-    #[test]
-    fn handle_key_event() {
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Right.into());
-        assert_eq!(app.counter, 1);
-
-        app.handle_key_event(KeyCode::Left.into());
-        assert_eq!(app.counter, 0);
-
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Char('q').into());
-        assert!(app.exit);
-    }
-}
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use ratatui::style::Style;
+//
+//     #[test]
+//     fn render() {
+//         let app = App::default();
+//         let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
+//
+//         app.render(buf.area, &mut buf);
+//
+//         let mut expected = Buffer::with_lines(vec![
+//             "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
+//             "┃                    Value: 0                    ┃",
+//             "┃                                                ┃",
+//             "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
+//         ]);
+//         let title_style = Style::new().bold();
+//         let counter_style = Style::new().yellow();
+//         let key_style = Style::new().blue().bold();
+//         expected.set_style(Rect::new(14, 0, 22, 1), title_style);
+//         expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
+//         expected.set_style(Rect::new(13, 3, 6, 1), key_style);
+//         expected.set_style(Rect::new(30, 3, 7, 1), key_style);
+//         expected.set_style(Rect::new(43, 3, 4, 1), key_style);
+//
+//         assert_eq!(buf, expected);
+//     }
+//
+//     #[test]
+//     fn handle_key_event() {
+//         let mut app = App::default();
+//         app.handle_key_event(KeyCode::Right.into());
+//         assert_eq!(app.counter, 1);
+//
+//         app.handle_key_event(KeyCode::Left.into());
+//         assert_eq!(app.counter, 0);
+//
+//         let mut app = App::default();
+//         app.handle_key_event(KeyCode::Char('q').into());
+//         assert!(app.exit);
+//     }
+// }
