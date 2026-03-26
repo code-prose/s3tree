a93205f916e30e3eec163fbbc9ed4116f6364250
Logan Rose <logprose@umich.edu>
2026-03-26 12:53:09 -0600

    feat: additional scenario

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@ source .dev.sh
 aws s3 mb s3://test-bucket
 
 aws s3 cp ./example-nested.csv s3://test-bucket/nested/example-nested.csv
+aws s3 cp ./example-nested.csv s3://test-bucket/nested/example-shared-nested.csv
 aws s3 cp ./example.txt s3://test-bucket/example.txt
+
